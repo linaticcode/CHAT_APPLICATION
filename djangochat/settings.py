fa3df5e5a59ed8aa617932c6beac0918bef3bc63
linaticcode <ss21eeb0a59@student.nitw.ac.in>
2023-07-16 14:00:16 +0530

    updated changes

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9epa)pbam-11!b3!)g9hbp^h5844wg+k6b3^t)b=#&m691l)nf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app' , '.now.sh' , '127.0.0.1' , 'localhost']
+ALLOWED_HOSTS = ['.vercel.app'  , '127.0.0.1' , 'localhost']
 
 
 # Application definition
